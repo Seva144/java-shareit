290b1f1e6c5a87a0b96eb9ef838bef0d0dc92ded
Артем Севостьянов <artemsevostanov@MacBook-Air-Artem.local>
2023-03-21 23:44:05 +0300

    New

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.user.model.User;
-
 import java.util.List;
 
 public interface UserRepository {
