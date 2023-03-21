d12aa999d66502fba62c6e381f2f15ccb24bd69d
Артем Севостьянов <artemsevostanov@MacBook-Air-Artem.local>
2023-03-21 23:36:12 +0300

    NEW branch

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.exceptions.ValidationException;
 import ru.practicum.shareit.user.dto.UserDto;
-
 import javax.validation.Valid;
 import java.util.List;
 
