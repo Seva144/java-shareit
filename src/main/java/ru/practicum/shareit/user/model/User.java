7e50638b54e0732d5594ba0844d42052bb6dedb4
Артем Севостьянов <artemsevostanov@MacBook-Air-Artem.local>
2023-04-15 21:53:12 +0300

    add-booking4

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,12 +3,8 @@ package ru.practicum.shareit.user.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.item.model.Item;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
